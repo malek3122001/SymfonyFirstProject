07a119906ba04dee586d71692cefc9894cf39b91
malek3122001 <benkhalifamalek29@gmail.com>
2023-10-22 20:09:10 +0100

    first commit

---
+++ src/Form/EtudiantType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etudiant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtudiantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('age')
+            ->add('classroom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Etudiant::class,
+        ]);
+    }
+}
